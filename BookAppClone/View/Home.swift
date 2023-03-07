7298b79540bf264292588b5bd3f6b6ef78d0c6a6
bozkurtnecati <bozkurtnecati07@hotmail.com>
2023-03-07 15:54:40 +0300

    Building Home View

--- BookAppClone/View/Home.swift
+++ BookAppClone/View/Home.swift
@@ -9,12 +9,24 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 15.0) {
+            HStack {
+                Text("Browse")
+                    .font(.largeTitle.bold())
+                Text("Recommended")
+                    .fontWeight(.semibold)
+                    .padding(.leading, 15)
+                    .foregroundColor(.gray)
+                    .offset(y: 2)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, 15)
+        }
     }
 }
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
-        Home()
+        ContentView()
     }
 }
