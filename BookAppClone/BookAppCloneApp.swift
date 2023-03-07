020862192ae0e8f8934bc4c82b73f5f06d16697b
bozkurtnecati <bozkurtnecati07@hotmail.com>
2023-03-07 15:22:04 +0300

    Initial Commit

---
+++ BookAppClone/BookAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAppCloneApp.swift
+//  BookAppClone
+//
+//  Created by Necati Bozkurt on 7.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
