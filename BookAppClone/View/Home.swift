a7f885a1c6a7227e977251c0ff66dea23fa75d2e
bozkurtnecati <bozkurtnecati07@hotmail.com>
2023-03-09 17:52:39 +0300

    Building Book's Card View

--- BookAppClone/View/Home.swift
+++ BookAppClone/View/Home.swift
@@ -27,7 +27,92 @@ struct Home: View {
             .padding(.horizontal, 15)
             
             TagsView()
+            
+            ScrollView(.vertical, showsIndicators: false) {
+                /// Books Card View
+                VStack(spacing: 35.0){
+                    ForEach(sampleBooks){
+                        BookCardView($0)
+                    }
+                }
+                .padding(.horizontal, 15)
+                .padding(.vertical, 20)
+            }
+            /// Since we need ofset from here and not from global view
+            .coordinateSpace(name: "SCROLLVIEW")
         }
+    }
+    
+    /// Books Card View
+    @ViewBuilder
+    func BookCardView(_ book: Book)-> some View {
+        GeometryReader{
+            let size = $0.size
+            let frame = $0.frame(in: .named("SCROLLVIEW"))
+            
+            HStack(spacing: -25){
+                /// Book Detail Card
+                /// Placing this card above the cover image
+                VStack(alignment: .leading, spacing: 6.0){
+                    Text(book.title)
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                    
+                    Text("By \(book.author)")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                    
+                    /// Rating View
+                    RatingView(rating: book.rating)
+                        .padding(.top, 10)
+                     
+                    Spacer(minLength: 10)
+                    
+                    HStack(spacing: 4.0){
+                        Text("\(book.bookViews)")
+                            .font(.caption)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.blue)
+                        
+                        Text("Views")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        
+                        Spacer(minLength: 0)
+                        
+                        Image(systemName: "chevron.right")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    
+                }
+                .padding(20)
+                .frame(width: size.width / 2, height: size.height * 0.8)
+                .background{
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .fill(.white)
+                        /// Applying Shadow
+                        .shadow(color: .black.opacity(0.08), radius: 8, x: 5, y: 5)
+                        .shadow(color: .black.opacity(0.08), radius: 8, x: -5, y: -5)
+                }
+                .zIndex(1)
+                
+                /// Book Cover Image
+                ZStack{
+                    Image(book.imageName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: size.width / 2, height: size.height)
+                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                        /// Applying Shadow
+                        .shadow(color: .black.opacity(0.1), radius: 5, x: 5, y: 5)
+                        .shadow(color: .black.opacity(0.1), radius: 5, x: -5, y: -5)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+            .frame(width: size.width)
+        }
+        .frame(height: 220)
     }
     
     /// Tag View
@@ -70,5 +155,25 @@ var tags: [String] = ["History","Classical","Biography","Cartoon","Adventure","F
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+/// Custom Rating View
+struct RatingView: View {
+    var rating: Int
+    var body: some View{
+        HStack(spacing: 4.0){
+            ForEach(1...5, id: \.self) { index in
+                Image(systemName: "star.fill")
+                    .font(.caption2)
+                    .foregroundColor(index <= rating ? .yellow : .gray.opacity(0.5))
+            }
+            
+            Text("(\(rating))")
+                .font(.caption)
+                .fontWeight(.semibold)
+                .foregroundColor(.yellow)
+                .padding(.leading)
+        }
     }
 }
